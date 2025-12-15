d55ae9c696538b47c4b88af2a2c9645871df1f17
vkp000 <vivekprajapatvkp0@gmail.com>
2025-12-15 12:33:06 +0530

    Add Selection Sorting implementation and update folder structure

---
+++ StriverA2Z/Revision/Sorting/SelectionSorting.java
@@ -0,0 +1,22 @@
+//  https://www.geeksforgeeks.org/problems/selection-sort/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=selection-sort
+
+
+class Solution {
+    void selectionSort(int[] arr) {
+        // code here
+        for (int i = 0; i < arr.length - 1; i++) {
+            int key = i;
+            for (int j = i + 1; j < arr.length; j++) {
+
+                if (arr[j] < arr[key]) {
+                    key = j;
+                }
+
+            }
+            int temp = arr[key];
+            arr[key] = arr[i];
+            arr[i] = temp;
+
+        }
+    }
+}
